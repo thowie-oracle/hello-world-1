b70b4e67b0291773e34b2de34e7fd2faa8b05652
Micha Hernandez van Leuffen <mies@wercker.com>
2017-08-11 15:37:55 +0200

    test

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gorilla/handlers"
-    "os"
-    "fmt"
+	"fmt"
+	"github.com/gorilla/handlers"
+	"net/http"
+	"os"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, World!"))
+	w.Write([]byte("Hello, World!@"))
 }
 
 func main() {
-    fmt.Printf("Starting Hello World application... \n")
-    http.HandleFunc("/", HelloWorld)
-    http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+	fmt.Printf("Starting Hello World application... \n")
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
 }
