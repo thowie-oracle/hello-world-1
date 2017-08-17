abee406f5b3b0126f4225381f7787da706825c04
wercker-demos <walter+demos@wercker.com>
2017-08-17 16:40:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World! Where are you? I'm here!"))
+	w.Write([]byte("Hello, World! Where are you? Hello!"))
 }
 
 func main() {
