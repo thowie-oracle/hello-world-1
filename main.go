9cdf93ff91826db35dd7672d127651299794d20a
Jon Reeve <reeve.jonathan@gmail.com>
2017-09-17 15:59:46 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello friendssss full run!!!!!"))
+	w.Write([]byte("Hello friendssss full run 2!!!!!"))
 }
 
 func main() {
