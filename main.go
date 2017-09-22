f0adfbe434cbd9c7cd95d850ff5aa6e84e8e9b61
Tim Howie <tim.howie@oracle.com>
2017-09-22 15:11:58 -0400

    Updated welcome message

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Open World peeps!!!!!"))
+	w.Write([]byte("Hello again, Open World peeps!!!!!"))
 }
 
 func main() {
